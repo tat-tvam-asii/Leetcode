1195ba7188a2b7a5bc872dbb775bd742d29aef56
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-03-11 22:38:06 +0530

    Time: 3 ms (50.27%) | Memory: 7.5 MB (66.00%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -3,7 +3,7 @@ public:
     string customSortString(string order, string s) {
         int arr[26] = {0};
         for(int i=0;i<order.length();i++) arr[order[i]-'a'] = i;
-        sort(s.begin(),s.end(),[arr](char a,char b) -> bool{
+        sort(s.begin(),s.end(),[&](char a,char b) -> bool{
             return arr[a-'a'] < arr[b-'a'];
         });
         return s;
