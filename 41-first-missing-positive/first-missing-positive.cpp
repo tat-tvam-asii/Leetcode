d2839b3c06913a52ad2376d39159067934720e88
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-03-17 23:17:46 +0530

    Time: 50 ms (34.70%) | Memory: 43.8 MB (34.90%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& A) {
-        if(A.size() == 1){
-            if(A[0] == 1) return 2;
-            else return 1;
-        }
+        A.push_back(0);
         for(int& i : A){
-            if(i < 0) i = INT_MIN; 
+            if(i < 0 || i >= A.size()) i = 0; 
         }
-        for(int i=0;i<A.size();i++){
-            if(A[i] != INT_MIN){
-                if(abs(A[i]) < A.size()){
-                    if(A[abs(A[i])] == INT_MIN) A[abs(A[i])] = -abs(A[i]);
-                    else{
-                        if(A[abs(A[i])] > 0) A[abs(A[i])] *= -1;
-                        else if(A[abs(A[i])] == 0) A[abs(A[i])] = -abs(A[i]);
-                    } 
-                }
-            }
+        int n = A.size();
+        for(int i=0;i<n;i++){
+            A[A[i]%n] += n;
         }
-        int i;
-        for(i=1;i<A.size();i++){
-            if(A[i] >= 0 || A[i] == INT_MIN) return i;
+        for(int j=1;j<n;j++){
+            if(A[j] < n) return j;
         }
-        for(int j=0;j<A.size();j++){
-            if(abs(A[j]) == i) return i+1;
-        }
-        return i;
+        return n;
     }
 };
