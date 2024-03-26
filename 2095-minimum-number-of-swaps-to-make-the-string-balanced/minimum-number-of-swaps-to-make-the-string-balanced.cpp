94a80880858f2917fdf541bd7b21f65b6a7c0f15
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-03-26 16:24:53 +0530

    Time: 110 ms (38.94%) | Memory: 32.4 MB (69.10%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int n = s.size();
-        int balance = 0, swaps = 0, j=n-1;
-        for(int i=0; i<n; i++)
-        {
-            if(s[i] == '[') balance++;
-            else balance--;
-
-            if(balance < 0)
-            {
-                while(i<j && s[j] != '[') j--;
-                swap(s[i], s[j]);
-                swaps++;
-                balance = 1;
-            }
+        int size = 0;
+        for(char &ch : s) {
+            if(ch == '[')      size++;
+            else if(size != 0) size--;
         }
-        return swaps;
+        return (size+1)/2;
     }
 };
