1c7c233bfc8a26bf7fbd35203dc1a570ab993f8f
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-03-31 12:27:18 +0530

    Time: 92 ms (46.81%) | Memory: 82.6 MB (53.99%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& A, int minK, int maxK) {
-        long res = 0, jbad = -1, jmin = -1, jmax = -1, n = A.size();
-        for (int i = 0; i < n; ++i) {
-            if (A[i] < minK || A[i] > maxK) jbad = i;
-            if (A[i] == minK) jmin = i;
-            if (A[i] == maxK) jmax = i;
-            res += max(0L, min(jmin, jmax) - jbad);
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long res = 0;
+        bool minFound = false, maxFound = false;
+        int start = 0, minStart = 0, maxStart = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            if (num < minK || num > maxK) {
+                minFound = false;
+                maxFound = false;
+                start = i+1;
+            }
+            if (num == minK) {
+                minFound = true;
+                minStart = i;
+            }
+            if (num == maxK) {
+                maxFound = true;
+                maxStart = i;
+            }
+            if (minFound && maxFound) {
+                res += (min(minStart, maxStart) - start + 1);
+            }
         }
         return res;
     }
