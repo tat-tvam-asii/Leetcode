9f36cbd5ce05a5e73813a31e46085508bc053c31
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-04-07 09:36:49 +0530

    Time: 3 ms (45.50%) | Memory: 7.2 MB (92.84%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<int> st,pa;
-        for(int i = 0;i < s.length();i++){
-            if(s[i] == '(') pa.push(i);
-            else if(s[i] == ')'){
-                if(!pa.empty()) pa.pop();
-                else if(!st.empty()) st.pop();
-                else return false;
-            }
-            else st.push(i);
+        int cmin = 0, cmax = 0;
+        for (char c : s) {
+            if (c == '(')
+                cmax++, cmin++;
+            if (c == ')')
+                cmax--, cmin--;
+            if (c == '*')
+                cmax++, cmin--;
+            cmin = max(cmin,0);
+            if (cmax < 0) return false;
         }
-        while(!pa.empty()){
-            if(st.empty() || pa.top() > st.top()) return false;
-            else if(pa.top() < st.top()){
-                pa.pop();
-                st.pop();
-            }
-        }
-        return true;
+        return cmin == 0;
     }
 };
