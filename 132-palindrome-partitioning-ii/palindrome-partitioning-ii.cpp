7cc7f86c3e4bedbea636cfa6a095142945b0d36f
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-06-23 14:56:12 +0530

    Time: 1751 ms (19.11%) | Memory: 7.5 MB (95.05%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,31 +1,27 @@
-class Solution {
+class Solution{
 public:
-    int dp[2001][2001];
-    
-    bool isPalindrome(string& str,int i,int j){
+    bool isPalin(string &s, int i, int j){
         while(i <= j){
-            if(str[i++] != str[j--]) return false;
+            if(s[i++] != s[j--]) return false;
         }
         return true;
     }
 
-    int solve(string& str,int i,int j){
-        if(i >= j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(isPalindrome(str,i,j)) return 0;
-        int mn = 1e9;
-        for(int k = i;k < j;k++){
-            if(isPalindrome(str,i,k)){
-                int partitions = 1 + solve(str,k+1,j);
-                if(partitions < mn) mn = partitions;
+    int minCut(string str){
+        int N = str.size();
+        vector<int> t(N+1,0);
+        t[N] = 0;
+        
+        for(int i=N-1;i>=0;i--){
+            int mini = INT_MAX;
+            for(int j=i;j<N;j++){
+                if(isPalin(str, i, j)){
+                    int ans = 1 + t[j+1];
+                    mini = min(mini, ans);
+                }
+                t[i] = mini;
             }
         }
-        return dp[i][j] = mn;
-    }
-
-    int minCut(string str)
-    {
-        memset(dp,-1,sizeof(dp));
-        return solve(str,0,str.length()-1);
+        return t[0] - 1;
     }
 };
