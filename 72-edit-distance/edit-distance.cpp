7ead53adde44d63b51fd934ca9a7c3c80cf04a72
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-07-10 17:36:19 +0530

    Time: 3 ms (92.59%) | Memory: 9.2 MB (98.51%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int minDistance(string s1, string s2) {
-        int n = s1.length(),m = s2.length();
-        vector<int> prev(m+1,0),curr(m+1,0);
-        for(int i = 0;i <= m;i++) prev[i] = i;
+        int n = s1.length(),m = s2.length(),prev;
+        vector<int> curr(m+1,0);
+        for(int i = 0;i <= m;i++) curr[i] = i;
         for(int i = 1;i <= n;i++){
+            prev = curr[0];
             curr[0] = i;
             for(int j = 1;j <= m;j++){
-                if(s1[i-1] == s2[j-1]) curr[j] = prev[j-1];
-                else curr[j] = 1 + min({prev[j],prev[j-1],curr[j-1]});
+                int temp =  curr[j];
+                if(s1[i-1] == s2[j-1]) curr[j] = prev;
+                else curr[j] = 1 + min({curr[j],curr[j-1],prev});
+                prev = temp;
             }
-            prev = curr;
         }
-        return prev[m];
+        return curr[m];
     }
 };
