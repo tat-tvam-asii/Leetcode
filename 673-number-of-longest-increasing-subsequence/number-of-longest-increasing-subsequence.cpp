7ba2fad4eac355c1e6d1a9eca27f0083c2e7b3f5
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-07-21 21:47:27 +0530

    Time: 32 ms (82.75%) | Memory: 16 MB (20.92%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> length(n,1),num(n,0);
+        vector<int> length(n,1),cnt(n,1);
         for(int i = 0;i < n;i++){
             for(int j = 0;j < i;j++){
-                if(nums[j] < nums[i]) length[i] = max(length[i],length[j]+1);
+                if(nums[j] < nums[i]){
+                    if (length[j] + 1 > length[i]) {
+                        length[i] = length[j] + 1;
+                        cnt[i] = 0;
+                    }
+                    if(length[j] == length[i] - 1) cnt[i] += cnt[j];
+                }
             }
-            for(int j = 0;j < i;j++){
-                if(length[j] == length[i]-1 && nums[j] < nums[i]) num[i] += num[j];
-            }
-            num[i] = max(num[i],1);
         }
         int mx = *max_element(length.begin(),length.end());
         int ans = 0;
         for(int i = 0;i < n;i++){
-            if(length[i] == mx) ans += num[i];
+            if(length[i] == mx) ans += cnt[i];
         }
         return ans;
     }
