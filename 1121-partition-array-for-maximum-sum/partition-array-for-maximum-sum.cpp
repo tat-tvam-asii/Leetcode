a4c442488ad349178225b96427c1d018f8a0e95b
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-07-22 18:07:24 +0530

    Time: 216 ms (5.14%) | Memory: 11 MB (23.59%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> dp(n+1);
         for(int i = 1;i <= n;i++){
             for(int j = 1;j <= min(k,i);j++){
-                if(i - j >= 0) dp[i] = max(dp[i],(dp[i-j] + *max_element(arr.begin()+i-j,arr.begin()+i)*j));
+                dp[i] = max(dp[i],(dp[i-j] + *max_element(arr.begin()+i-j,arr.begin()+i)*j));
             }
         }
         return dp[n];
