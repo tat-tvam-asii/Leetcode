6a333d8e47f75afe33f7cfd1390dc8578daeb2ae
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-08-24 23:32:05 +0530

    Time: 21 ms (48.15%) | Memory: 20.2 MB (54.06%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    
-    int numDistinct(string s, string t) {
-        int m = s.length(),n = t.length();
-        vector<vector<unsigned long long>> dp(m+1,vector<unsigned long long>(n+1,0));
-        for(int i = 0;i <= m;i++) dp[i][n] = 1;
-        for(int i = m-1;i >= 0;i--){
-            for(int j = n-1;j >= 0;j--){
-                dp[i][j] = dp[i+1][j];
-                if(s[i] == t[j]) dp[i][j] += dp[i+1][j+1];
-            }
+    int solve(string& s,string& t,int i,int j,vector<vector<int>>& dp){
+        if(j == t.length()){
+            return 1;
         }
-        return dp[0][0];
+        if(i == s.length()) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        dp[i][j] = solve(s,t,i+1,j,dp);
+        if(s[i] == t[j]) dp[i][j] += solve(s,t,i+1,j+1,dp);
+        return dp[i][j];
+    }
+
+    int numDistinct(string s, string t) {
+        vector<vector<int>> dp(s.length(),vector<int>(t.length(),-1));
+        return solve(s,t,0,0,dp);
     }
 };
