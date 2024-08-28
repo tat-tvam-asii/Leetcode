fc69e939a47e3f15b8df091651efa081b8e21579
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-08-28 22:23:41 +0530

    Time: 87 ms (33.20%) | Memory: 149.2 MB (5.17%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.cpp
+++ 1332-count-vowels-permutation/count-vowels-permutation.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int mod = 1e9 + 7;
 
-    long long solve(int n,char i,vector<vector<int>>& dp){
-        if(n == 1) return 1;
-        int x = i - 'a';
-        if(dp[n][x] != -1) return dp[n][x];
-        if(i == 'a') return dp[n][x] = solve(n-1,'e',dp) % mod;
-        if(i == 'e') return dp[n][x] = (solve(n-1,'a',dp) + solve(n-1,'i',dp)) % mod;
-        if(i == 'i') return dp[n][x] = (solve(n-1,'a',dp) + solve(n-1,'e',dp) + solve(n-1,'u',dp) + solve(n-1,'o',dp)) % mod;
-        if(i == 'o') return dp[n][x] = (solve(n-1,'i',dp) + solve(n-1,'u',dp)) % mod;
-        if(i == 'u') return dp[n][x] = solve(n-1,'a',dp) % mod;
-        return 0;
-    }
-
     int countVowelPermutation(int n) {
-        vector<vector<int>> dp(n+1,vector<int>(26,-1));
-        return (solve(n,'a',dp) + solve(n,'e',dp) + solve(n,'i',dp) + solve(n,'o',dp) + solve(n,'u',dp)) % mod;
+        vector<vector<long long>> dp(n+1,vector<long long>(26,1));
+        string vowel = "aeiou";
+        for(int i = 2;i <= n;i++){
+            for(char j : vowel){
+                int x = j - 'a';
+                if(j == 'a') dp[i][x] = dp[i-1]['e'-'a'] % mod;
+                if(j == 'e') dp[i][x] = (dp[i-1]['a'-'a'] + dp[i-1]['i'-'a']) % mod;
+                if(j == 'i') dp[i][x] = (dp[i-1]['a'-'a'] + dp[i-1]['e'-'a'] + dp[i-1]['o'-'a'] + dp[i-1]['u'-'a']) % mod;
+                if(j == 'o') dp[i][x] = (dp[i-1]['i'-'a'] + dp[i-1]['u'-'a']) % mod;
+                if(j == 'u') dp[i][x] = dp[i-1]['a'-'a'] % mod;
+            }
+        }
+        int ans = 0;
+        for(char i : vowel) ans = (ans + dp[n][i-'a']) % mod;
+        return ans;
     }
 };
