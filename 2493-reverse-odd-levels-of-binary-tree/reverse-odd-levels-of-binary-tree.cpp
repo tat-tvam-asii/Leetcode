ac0276a60218b230c704cb8367e4ebbc2816af58
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-12-26 11:53:33 +0530

    Time: 0 ms (100.00%) | Memory: 77.3 MB (69.22%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -1,41 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        int level = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        vector<int> temp;
-        while(!q.empty()){
-            int n = q.size(),j = 0;
-            vector<int> temp1;
-            for(int i = 0;i < n;i++){
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->right){
-                    q.push(node->right);
-                    temp1.push_back(node->right->val);
-                }
-                if(node->left){
-                    q.push(node->left);
-                    temp1.push_back(node->left->val);
-                }
-                if(level % 2) node->val = temp[j++];
-            }
-            if(level % 2 == 0) reverse(temp1.begin(),temp1.end());
-            temp = temp1;
-            level++;
-        }
+        traverseDFS(root->left, root->right, 0);
         return root;
+    }
+
+private:
+    void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
+        if (leftChild == nullptr || rightChild == nullptr) {
+            return;
+        }
+        if (level % 2 == 0) {
+            int temp = leftChild->val;
+            leftChild->val = rightChild->val;
+            rightChild->val = temp;
+        }
+
+        traverseDFS(leftChild->left, rightChild->right, level + 1);
+        traverseDFS(leftChild->right, rightChild->left, level + 1);
     }
 };
