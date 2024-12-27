6538cf1fa43efd1290cea86399a2837ba1509f21
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2024-12-27 20:55:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (65.45%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int n = values.size();
-        vector<int> lastmax(n);
-        lastmax[n-1] = values[n-1] - (n-1);
-        for(int i = n-2;i >= 0;i--) lastmax[i] = max(lastmax[i+1],values[i] - i);
-        int mx = INT_MIN;
-        for(int i = 0;i < n-1;i++) mx = max(mx,values[i] + i + lastmax[i+1]);
-        return mx;
+        int left = values[0],ans = INT_MIN;
+        for(int i = 1;i < values.size();i++){
+            ans = max(ans,left + values[i] - i);
+            left = max(left,values[i] + i);
+        }
+        return ans;
     }
 };
