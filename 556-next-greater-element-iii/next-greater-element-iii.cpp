fa3cbbf862bd3ee46b490afc438ef18f3bce67ef
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-01-24 11:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (61.44%) - LeetSync

---
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        if(n < 12) return -1;
+        string s = to_string(n);
+        int l = s.length();
+        for(int i = l-2;i >= 0;i--){
+            char c = '@';
+            int ind = i;
+            for(int j = i+1;j < l;j++){
+                if(s[j] > s[i]){
+                    if(s[j] < c){
+                        c = s[j];
+                        ind = j;
+                    }
+                }
+            }
+            if(c != '@'){
+                swap(s[i],s[ind]);
+                sort(s.begin()+i+1,s.end());
+                long int i = stol(s);
+                if(i > INT_MAX) return -1;
+                return stoi(s);
+            }
+        }
+        return -1;
+    }
+};
