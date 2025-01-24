86c6d7bf981b33f59880a86e08b4f91231ab4cb2
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-01-24 11:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (61.44%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -3,25 +3,17 @@ public:
     int nextGreaterElement(int n) {
         string s = to_string(n);
         int l = s.length();
-        for(int i = l-2;i >= 0;i--){
-            char c = '@';
-            int ind = i;
-            for(int j = i+1;j < l;j++){
-                if(s[j] > s[i]){
-                    if(s[j] < c){
-                        c = s[j];
-                        ind = j;
-                    }
-                }
-            }
-            if(c != '@'){
-                swap(s[i],s[ind]);
-                sort(s.begin()+i+1,s.end());
-                long int i = stol(s);
-                if(i > INT_MAX) return -1;
-                return stoi(s);
+        int i = l - 2;
+        while(i >= 0 && s[i] >= s[i+1]) i--;
+        if(i == -1) return -1;
+        reverse(s.begin() + i + 1,s.end());
+        for(int j = i + 1;j < l;j++){
+            if(s[j] > s[i]){
+                swap(s[i],s[j]);
+                break;
             }
         }
-        return -1;
+        long ans = stol(s);
+        return ans == n || ans > INT_MAX ? -1 : ans;
     }
 };
