41af04d177967e624ec04753f8d9c7da7ceb9deb
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-01-26 19:40:18 +0530

    Time: 3 ms (20.53%) | Memory: 27.4 MB (9.36%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,29 +2,16 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> ngl(n);
-        stack<int> stgl;
-        for(int i = 0;i < n;i++){
-            while(!stgl.empty() && stgl.top() <= height[i]) stgl.pop();
-            if(stgl.empty()){
-                ngl[i] = 0;
-                stgl.push(height[i]);
-            }
-            else ngl[i] = stgl.top();
-        }
-        vector<int> ngr(n);
-        stack<int> stgr;
-        for(int i = n-1;i >= 0;i--){
-            while(!stgr.empty() && stgr.top() <= height[i]) stgr.pop();
-            if(stgr.empty()){
-                ngr[i] = 0;
-                stgr.push(height[i]);
-            }
-            else ngr[i] = stgr.top();
-        }
+        vector<int> leftMax(n), rightMax(n);
+        for (int i = 1; i < n; ++i) 
+            leftMax[i] = max(height[i-1], leftMax[i-1]);
+        for (int i = n-2; i >= 0; --i) 
+            rightMax[i] = max(height[i+1], rightMax[i+1]);
+        
         int ans = 0;
-        for(int i = 0;i < n;i++){
-            ans += max((min(ngr[i],ngl[i]) - height[i]),0);
+        for (int i = 0; i < n; ++i) {
+            int waterLevel = min(leftMax[i], rightMax[i]);
+            if (waterLevel >= height[i]) ans += waterLevel - height[i];
         }
         return ans;
     }
