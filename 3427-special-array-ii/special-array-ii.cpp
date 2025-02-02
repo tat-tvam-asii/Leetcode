3e99bc8a5bd48f2aa303b6316cd9d33f3cb4c478
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-02-02 17:10:31 +0530

    Time: 115 ms (7.26%) | Memory: 140.4 MB (5.53%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        unordered_set<int> faults;
+        set<int> s;
         for(int i = 0;i < n-1;i++){
-            if((nums[i] + nums[i+1]) % 2 == 0) faults.insert(i);
+            if((nums[i] + nums[i+1]) % 2 == 0) s.insert(i);
         }
         vector<bool> ans;
         for(auto i : queries){
-            bool flag = true;
-            for(int n : faults){
-                if(n >= i[0] && n < i[1]){
-                    flag = false;
-                    ans.push_back(false);
-                    break;
-                }
-            }
-            if(flag) ans.push_back(true);
+            if(s.lower_bound(i[0]) == s.lower_bound(i[1])) ans.push_back(true);
+            else ans.push_back(false);
         }
         return ans;
     }
