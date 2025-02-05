351502e16096daa69ef7f480c2bdb0aa0f14a732
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-02-05 22:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (7.40%) - LeetSync

--- 738-monotone-increasing-digits/monotone-increasing-digits.cpp
+++ 738-monotone-increasing-digits/monotone-increasing-digits.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int monotoneIncreasingDigits(int n) {
-        string s = to_string(n);
-        string ans = "";
+        string s = to_string(n),ans;
         int l = s.length();
         for(int i = l-1;i >= 0;i--){
             if(i > 0 && s[i] < s[i-1]){
-                string temp = string(ans.length()+1,'9');
-                ans = temp;
+                ans = string(ans.length()+1,'9');
                 s[i-1] -= 1;
             }
             else ans += s[i];
