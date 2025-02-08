bc19107427d85ccb9accf9e21c2589477b5184d8
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-02-08 16:09:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (28.70%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> freq(26,0);
+        for(char c : s) freq[c-'a']++;
+        stack<char> st;
+        unordered_map<char,int> mp;
+        for(char c : s){
+            if(mp[c]){
+                freq[c-'a']--;
+                continue;
+            }
+            while(!st.empty() && c < st.top() && freq[st.top()-'a'] > 1){
+                freq[st.top()-'a']--;
+                mp[st.top()]--;
+                st.pop();
+            }
+            st.push(c);
+            mp[c]++;
+        }
+        string ans;
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
