9fe5f6fda7a8bb8bc6fddc20b60e9e5d1932c862
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-02-11 16:48:52 +0530

    Time: 420 ms (5.23%) | Memory: 419 MB (6.89%) - LeetSync

--- 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
@@ -1,30 +1,10 @@
 class Solution {
 public:
-    string largestMerge(string w1, string w2) {
-        int n1 = w1.size();
-        int n2 = w2.size();
-        string ans;
-        int i = 0, j = 0;
-
-        while(i < n1 && j < n2){
-            if(w1[i] - w2[j]> 0){
-                ans.push_back(w1[i++]);
-            }
-            else if(w1[i] - w2[j]< 0){
-                ans.push_back(w2[j++]);
-            }
-            else{
-                string s1(begin(w1) + i, end(w1));
-                string s2(begin(w2) + j, end(w2));
-                if(s1 > s2) ans.push_back(w1[i++]);
-                else ans.push_back(w2[j++]);
-                }
-            }
-            
-
-        while(i < n1) ans.push_back(w1[i++]);
-        while(j < n2) ans.push_back(w2[j++]);
-
-        return ans;
+    string largestMerge(string s1, string s2) {
+        if (s1.size() == 0  || s2.size() == 0)
+            return s1 + s2;
+        if (s1 > s2)
+            return s1[0] + largestMerge(s1.substr(1), s2);
+        return s2[0] + largestMerge(s1, s2.substr(1));
     }
 };
