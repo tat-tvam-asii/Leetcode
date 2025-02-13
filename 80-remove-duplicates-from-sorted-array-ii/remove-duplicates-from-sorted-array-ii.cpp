99a81b64469f953936a66d295c72148f21701a4f
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-02-13 11:34:34 +0530

    Time: 7 ms (32.27%) | Memory: 19.6 MB (12.08%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -4,9 +4,7 @@ public:
         if(nums.size() <= 2) return nums.size();
         int k = 2;
         for(int i = 2; i < nums.size(); i++){      
-            if(nums[i] != nums[k-1]) nums[k++] = nums[i];    
-            else if(nums[i] == nums[k-1] && nums[i] != nums[k-2]) nums[k++] = nums[i]; 
-            else continue;
+            if(nums[i] != nums[k-2]) nums[k++] = nums[i];
         }
         return k;
     }
