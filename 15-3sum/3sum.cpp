bf4f1d13040fa49c60cab226e35005fd8f14e657
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-02-15 20:37:38 +0530

    Time: 39 ms (95.54%) | Memory: 29 MB (85.07%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,6 +5,7 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size(); i++) {
+            if(nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
