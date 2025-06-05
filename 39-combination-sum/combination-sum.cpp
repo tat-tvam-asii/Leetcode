886dc7756685113ddb8a8acf4e6cd8b2e365f1ca
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-06-05 10:34:07 +0530

    Time: 3 ms (56.02%) | Memory: 14.2 MB (54.66%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& ans,vector<int>& subseq,set<vector<int>>& st,vector<int>& candidates,int target,int sum,int ind){
+    void solve(vector<vector<int>>& ans,vector<int>& subseq,vector<int>& candidates,int target,int sum,int ind){
         if(sum == target){
-            if(st.contains(subseq)) return;
             ans.push_back(subseq);
-            st.insert(subseq);
             return;
         }
         if(ind == candidates.size()) return;
         if(sum + candidates[ind] > target) return;
         subseq.push_back(candidates[ind]);
         sum += candidates[ind];
-        solve(ans,subseq,st,candidates,target,sum,ind);
+        solve(ans,subseq,candidates,target,sum,ind);
         subseq.pop_back();
         sum -= candidates[ind];
-        solve(ans,subseq,st,candidates,target,sum,ind+1);
+        solve(ans,subseq,candidates,target,sum,ind+1);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> subseq;
-        set<vector<int>> st;
         sort(candidates.begin(),candidates.end());
-        solve(ans,subseq,st,candidates,target,0,0);
+        solve(ans,subseq,candidates,target,0,0);
         return ans;
     }
 };
