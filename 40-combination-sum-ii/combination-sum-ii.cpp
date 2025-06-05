371e17a7df1b69415318976b6ac2d7950cf74ac8
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-06-05 12:22:11 +0530

    Time: 1 ms (53.55%) | Memory: 14.5 MB (24.04%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& ans,vector<int>& subseq,set<vector<int>>& st,vector<int>& candidates,int target,int sum,int ind){
+        if(sum == target){
+            if(st.contains(subseq)) return;
+            ans.push_back(subseq);
+            st.insert(subseq);
+            return;
+        }
+        if(ind == candidates.size()) return;
+        if(sum + candidates[ind] > target) return;
+        subseq.push_back(candidates[ind]);
+        sum += candidates[ind];
+        solve(ans,subseq,st,candidates,target,sum,ind+1);
+        subseq.pop_back();
+        sum -= candidates[ind];
+        int temp = candidates[ind];
+        while(ind < candidates.size() && candidates[ind] == temp) ind++;
+        solve(ans,subseq,st,candidates,target,sum,ind);
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> subseq;
+        set<vector<int>> st;
+        sort(candidates.begin(),candidates.end());
+        solve(ans,subseq,st,candidates,target,0,0);
+        return ans;
+    }
+};
