8c19efdc9deee2afb4d23ea2adecadd946e04771
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-07-13 20:15:33 +0530

    Time: 55 ms (43.45%) | Memory: 13.4 MB (96.11%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-
-    int solve(int i,int j1,int j2,int r,int c,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp){
-        if(j1 < 0 || j1 >= c || j2 < 0 || j2 >= c) return -1e8;
-        
-        if(i == r-1){
-            if(j1 == j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
-        }
-
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-
-        int maxi = INT_MIN;
-        for(int di = -1;di <= 1;di++){
-            for(int dj = -1;dj <= 1;dj++){
-                int ans;
-                if(j1 == j2) ans = grid[i][j1];
-                else ans = grid[i][j1] + grid[i][j2];
-                ans += solve(i+1,j1+di,j2+dj,r,c,grid,dp);
-                maxi = max(maxi,ans);
-            }
-        }
-        return dp[i][j1][j2] = maxi;
-    }
-
     int cherryPickup(vector<vector<int>>& grid) {
         int r = grid.size(),c = grid[0].size();
-        vector<vector<vector<int>>> dp(r,vector<vector<int>>(c,vector<int>(c,-1)));
-        return solve(0,0,c-1,r,c,grid,dp);
+        vector<vector<int>> prev(c,vector<int>(c,-1));
+        vector<vector<int>> curr(c,vector<int>(c,-1));
+
+        for(int j1 = 0;j1 < c;j1++){
+            for(int j2 = 0;j2 < c;j2++){
+                if(j1 == j2) prev[j1][j2] = grid[r-1][j1];
+                else prev[j1][j2] = grid[r-1][j1] + grid[r-1][j2];
+            }
+        }
+
+        for(int i = r-2;i >= 0;i--){
+            for(int j1 = 0;j1 < c;j1++){
+                for(int j2 = 0;j2 < c;j2++){
+                    int maxi = INT_MIN;
+                    for(int di = -1;di <= 1;di++){
+                        for(int dj = -1;dj <= 1;dj++){
+                            int ans;
+                            if(j1 == j2) ans = grid[i][j1];
+                            else ans = grid[i][j1] + grid[i][j2];
+                            if(j1+di < 0 || j1+di >= c || j2+dj < 0 || j2+dj >= c) ans += -1e8;
+                            else ans += prev[j1+di][j2+dj];
+                            maxi = max(maxi,ans);
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            prev = curr;
+        }
+        return prev[0][c-1];
     }
 };
