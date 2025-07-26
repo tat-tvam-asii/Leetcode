c1f7619220f8673e8d52eb353f4a43bb5488c8f9
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-07-26 20:04:34 +0530

    Time: 3 ms (52.63%) | Memory: 9.2 MB (54.66%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool solve(string& s,string& p,int i,int j,vector<vector<int>>& dp){
-        if(i == 0){
-            while(j > 1 && p[j-1] == '*') j -= 2;
-            return j == 0;
-        }
-        if(j == 0) return i == 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(p[j-1] == '*') return dp[i][j] = solve(s,p,i,j-1,dp);
-        if(s[i-1] == p[j-1]){
-            if(j < p.length() && p[j] == '*') return dp[i][j] = solve(s,p,i-1,j-1,dp) || solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
-            else return dp[i][j] = solve(s,p,i-1,j-1,dp);
-        }
-        else{
-            if(p[j-1] == '.' && j < p.length() && p[j] == '*') return dp[i][j] = solve(s,p,i-1,j-1,dp) || solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
-            else if(p[j-1] == '.') return dp[i][j] = solve(s,p,i-1,j-1,dp);
-            else if(j < p.length() && p[j] == '*') return dp[i][j] = solve(s,p,i,j-1,dp);
-            else return dp[i][j] = false;
-        }
-    }
-
     bool isMatch(string s, string p) {
         int m = s.length(),n = p.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return solve(s,p,m,n,dp);
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+        dp[0][0] = true;
+        for(int j = 2;j <= n;j++){
+            if(p[j-1] == '*') dp[0][j] = dp[0][j-2];
+            else dp[0][j] = false;
+        }
+
+        for(int i = 1;i <= m;i++){
+            for(int j = 1;j <= n;j++){
+                if(s[i-1] == p[j-1]){
+                    if(j < p.length() && p[j] == '*') dp[i][j] = dp[i-1][j-1] || dp[i-1][j] || dp[i][j-1];
+                    else dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    if(p[j-1] == '*') dp[i][j] = dp[i][j-1];
+                    else if(p[j-1] == '.' && j < p.length() && p[j] == '*') dp[i][j] = dp[i-1][j-1] || dp[i-1][j] || dp[i][j-1];
+                    else if(p[j-1] == '.') dp[i][j] = dp[i-1][j-1];
+                    else if(j < p.length() && p[j] == '*') dp[i][j] = dp[i][j-1];
+                    else dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
