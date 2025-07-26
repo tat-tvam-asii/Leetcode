65d9f2279f1f4a6be2746409b59844533bca8ca9
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-07-26 18:31:21 +0530

    Time: 29 ms (72.57%) | Memory: 15.2 MB (55.01%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        string temp = "";
+        for(int i = 0;i < p.length();i++){
+            temp.push_back(p[i]);
+            while(i < p.length() - 1 && p[i] == '*' && p[i+1] == '*') i++;
+        }
+        p = temp;
         int m = s.length(),n = p.length();
         vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
         dp[0][0] = true;
-        for(int j = 1;j <= n;j++){
-            string temp(j,'*');
-            dp[0][j] = (temp == p.substr(0,j));
-        }
+        if(n && p[0] == '*') dp[0][1] = true;
         for(int i = 1;i <= m;i++){
             for(int j = 1;j <= n;j++){
                 if(s[i-1] == p[j-1]) dp[i][j] = dp[i-1][j-1];
