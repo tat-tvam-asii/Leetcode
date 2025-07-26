1f2c1a20e11c3b41f2f7109cf41e8adc76f7a58c
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-07-26 19:55:47 +0530

    Time: 4 ms (34.41%) | Memory: 9.7 MB (23.84%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    bool solve(string& s,string& p,int i,int j){
-        if(i < 0){
-            while(j > 0 && p[j] == '*') j -= 2;
-            return j < 0;
+    bool solve(string& s,string& p,int i,int j,vector<vector<int>>& dp){
+        if(i == 0){
+            while(j > 1 && p[j-1] == '*') j -= 2;
+            return j == 0;
         }
-        if(j < 0) return i < 0;
-        if(p[j] == '*') return solve(s,p,i,j-1);
-        if(s[i] == p[j]){
-            if(j < p.length() - 1 && p[j+1] == '*') return solve(s,p,i-1,j-1) || solve(s,p,i-1,j) || solve(s,p,i,j-1);
-            else return solve(s,p,i-1,j-1);
+        if(j == 0) return i == 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(p[j-1] == '*') return dp[i][j] = solve(s,p,i,j-1,dp);
+        if(s[i-1] == p[j-1]){
+            if(j < p.length() && p[j] == '*') return dp[i][j] = solve(s,p,i-1,j-1,dp) || solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
+            else return dp[i][j] = solve(s,p,i-1,j-1,dp);
         }
         else{
-            if(p[j] == '.' && j < p.length() - 1 && p[j+1] == '*') return solve(s,p,i-1,j-1) || solve(s,p,i-1,j) || solve(s,p,i,j-1);
-            else if(p[j] == '.') return solve(s,p,i-1,j-1);
-            else if(j < p.length() - 1 && p[j+1] == '*') return solve(s,p,i,j-1);
-            else return false;
+            if(p[j-1] == '.' && j < p.length() && p[j] == '*') return dp[i][j] = solve(s,p,i-1,j-1,dp) || solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
+            else if(p[j-1] == '.') return dp[i][j] = solve(s,p,i-1,j-1,dp);
+            else if(j < p.length() && p[j] == '*') return dp[i][j] = solve(s,p,i,j-1,dp);
+            else return dp[i][j] = false;
         }
     }
 
     bool isMatch(string s, string p) {
         int m = s.length(),n = p.length();
-        return solve(s,p,m-1,n-1);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(s,p,m,n,dp);
     }
 };
