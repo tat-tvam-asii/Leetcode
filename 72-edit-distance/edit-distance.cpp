960f4c0c30ae1b622edda87ec7c7db317ede4aa9
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-07-26 15:14:37 +0530

    Time: 4 ms (80.24%) | Memory: 14.5 MB (17.09%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int minDistance(string s1, string s2) {
-        int n = s1.length(),m = s2.length(),prev;
-        vector<int> curr(m+1,0);
-        for(int i = 0;i <= m;i++) curr[i] = i;
-        for(int i = 1;i <= n;i++){
-            prev = curr[0];
-            curr[0] = i;
-            for(int j = 1;j <= m;j++){
-                int temp =  curr[j];
-                if(s1[i-1] == s2[j-1]) curr[j] = prev;
-                else curr[j] = 1 + min({curr[j],curr[j-1],prev});
-                prev = temp;
-            }
-        }
-        return curr[m];
+    int solve(string& word1,string& word2,int i,int j,vector<vector<int>>& dp){
+        if(dp[i][j] != -1) return dp[i][j];
+        if(word1[i-1] == word2[j-1]) return dp[i][j] = solve(word1,word2,i-1,j-1,dp);
+        
+        int in = solve(word1,word2,i,j-1,dp);
+        int d = solve(word1,word2,i-1,j,dp);
+        int r = solve(word1,word2,i-1,j-1,dp);
+        return dp[i][j] = 1 + min({in,d,r});
+    }
+
+    int minDistance(string word1, string word2) {
+        int m = word1.length(),n = word2.length();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        for(int i = 0;i <= m;i++) dp[i][0] = i;
+        for(int j = 0;j <= n;j++) dp[0][j] = j;
+        return solve(word1,word2,m,n,dp);
     }
 };
