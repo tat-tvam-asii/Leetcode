56afc1b5006ab2ca0cb785c4e112aad02e66d972
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-07-26 17:51:02 +0530

    Time: 43 ms (13.42%) | Memory: 20.3 MB (53.31%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    bool solve(string& s,string& p,int i,int j,vector<vector<int>>& dp){
-        if(j == 0) return i == 0;
-        if(i == 0){
-            if(j == 0) return true;
-            else{
-                while(j > 0){
-                    if(p[j-1] != '*') return false;
-                    j--;
-                }
-                return true;
-            }
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i-1] == p[j-1]) return dp[i][j] = solve(s,p,i-1,j-1,dp);
-        else{
-            if(p[j-1] == '?') return dp[i][j] = solve(s,p,i-1,j-1,dp);
-            else if(p[j-1] == '*') return dp[i][j] = solve(s,p,i-1,j,dp) || solve(s,p,i-1,j-1,dp) || solve(s,p,i,j-1,dp);
-            else return dp[i][j] = false;
-        }
-    }
-
     bool isMatch(string s, string p) {
         int m = s.length(),n = p.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return solve(s,p,m,n,dp);
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+        dp[0][0] = true;
+        for(int j = 1;j <= n;j++){
+            string temp(j,'*');
+            dp[0][j] = (temp == p.substr(0,j));
+        }
+        for(int i = 1;i <= m;i++){
+            for(int j = 1;j <= n;j++){
+                if(s[i-1] == p[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    if(p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                    else if(p[j-1] == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1] || dp[i-1][j-1];
+                    else dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
