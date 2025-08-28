0ff0999af1a0c2e4de8978aedf34c03ba7ee407e
Yash Kumar <143332267+tat-tvam-asii@users.noreply.github.com>
2025-08-28 09:55:39 +0530

    Time: 20 ms (10.61%) | Memory: 38 MB (5.33%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
+    int solve(vector<int>& arr,int i,int j,int k,vector<vector<int>>& dp){
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int mx = INT_MIN,mxsum = 0;
+        for(int t = i;t < i+k && t < arr.size();t++){
+            mx = max(mx,arr[t]);
+            int sum = (t-i+1) * mx + solve(arr,t+1,j,k,dp);
+            mxsum = max(mxsum,sum);
+        }
+        return dp[i][j] = mxsum;
+    }
+
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n+1);
-        for(int i = 1;i <= n;i++){
-            int mx = 0;
-            for(int j = 1;j <= min(k,i);j++){
-                mx = max(mx,arr[i-j]);
-                dp[i] = max(dp[i],(dp[i-j] + mx*j));
-            }
-        }
-        return dp[n];
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(arr,0,n,k,dp);
     }
 };
